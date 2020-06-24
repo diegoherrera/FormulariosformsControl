0e8f2786a5812351f373a4f12fc88e722a8b1c97
Diego Sebastian Herrera <diherrer@everis.com>
2020-06-24 20:43:48 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Actor } from './Entidades/Actor';
+import { NgForm, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,31 @@ import { Actor } from './Entidades/Actor';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'router00';
-
+  title = 'Form';
   
-
+  Nombre = new FormControl('', [Validators.required, Validators.maxLength(15)]);
+  Apellido = new FormControl('', [Validators.required, Validators.maxLength(55)]);
+  Edad = new FormControl(20, Validators.required);
+  Pais = new FormControl({ value: 'Argentina', disabled: true });
+  Profesional = new FormControl(true);
   constructor() {
-    
+  }
+
+  setNameValue() {
+    this.Nombre.setValue('Donald Trump');
+    console.log('Name: ' + this.Nombre.value);
+    console.log('Estado: ' + this.Nombre.status);
+  }
+  setResetName() {
+    this.Nombre.reset();
+  }
+  changeValue() {
+    console.log(this.Pais.value);
+    this.Pais = new FormControl('EEUU');
+  }
+
+  accionformulario() {
+    console.log(this.Nombre.value);
+    console.log(this.Apellido.value);
   }
 }
